5260a408cb71cc341be2ee7061ba2f4f1f556295
Oita Alexandru <oita.alex@yahoo.com>
2020-02-25 23:47:43 +0200

    5

---
+++ src/main/java/NumberOfDays.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class NumberOfDays {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enther the month: ");
+        String month = scan.next();
+        switch(month){
+            case "January":
+                System.out.println("days = 31");
+                break;
+            case "February":
+                System.out.println("days = 28");
+                break;
+            case "March":
+                System.out.println("days = 31");
+                break;
+            case "April":
+                System.out.println("days = 30");
+                break;
+            case "May":
+                System.out.println("days = 31");
+                break;
+            case "June":
+                System.out.println("days = 30");
+                break;
+            case "July":
+                System.out.println("days = 31");
+                break;
+            case "August":
+                System.out.println("days = 31");
+                break;
+            case "September":
+                System.out.println("days = 30");
+                break;
+            case "October":
+                System.out.println("days = 28");
+                break;
+            case "November":
+                System.out.println("days = 30");
+                break;
+            case "December":
+                System.out.println("days = 31");
+                break;
+        }
+    }
+}
--- src/main/java/PrimeNumersLower.java
+++ src/main/java/PrimeNumersLower.java
@@ -1,8 +1,10 @@
-public class PrimeNumbersSum {
+import java.util.Scanner;
+
+public class PrimeNumersLower {
     public static void main(String args[]) {
-        int n = 10;
-        int k = 0;
-        int s = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = scan.nextInt();
         for (int i = 2; i <= n; i++) {
             boolean prime = true;
             for (int j = 2; j <= i / 2; j++) {
@@ -12,16 +14,8 @@ public class PrimeNumbersSum {
                 }
             }
             if (prime) {
-                //  System.out.println(i);
-                s = s + i;
-                k++;
-            }
-            if (k > 100) {
-                break;
-            } else {
-                n++;
+                System.out.println(i);
             }
         }
-        System.out.println(s);
     }
 }
